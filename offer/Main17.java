c1923201a9bec0d1cd4e2a9d1045863adc24673f
linsir <linsir678@gmail.com>
2018-04-02 14:47:39 +0800

    add code

--- offer/Main17.java
+++ offer/Main17.java
@@ -2,11 +2,31 @@ package offer;
 
 public class Main17 {
 
-    public void Mirror(TreeNode root) {
+    public static void Mirror(TreeNode root) {
+        if (root == null)
+            return;
 
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        Mirror(root.left);
+        Mirror(root.right);
 
 
     }
 
+
+    public static void main(String[] args) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+
+        t1.left = t2;
+        t1.right = t3;
+
+        Mirror(t1);
+        System.out.println(t1.left.val);
+
+    }
 
 }
