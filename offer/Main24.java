19a462931c882614443f8a9487ecfe4a07aa41fa
linsir <linsir678@gmail.com>
2018-04-07 00:46:13 +0800

    add main24

---
+++ offer/Main24.java
@@ -0,0 +1,58 @@
+package offer;
+
+public class Main24 {
+
+    static class RandomListNode {
+        int label;
+        RandomListNode next = null;
+        RandomListNode random = null;
+
+        RandomListNode(int label) {
+            this.label = label;
+        }
+    }
+
+    public RandomListNode Clone(RandomListNode pHead) {
+        RandomListNode p = pHead;
+        RandomListNode t = pHead;
+        while (p != null) {
+            RandomListNode q = new RandomListNode(p.label);
+            q.next = p.next;
+            p.next = q;
+            p = q.next;
+        }
+        while (t != null) {
+            RandomListNode q = t.next;
+            if (t.random != null)
+                q.random = t.random.next;
+            t = q.next;
+
+        }
+        RandomListNode s = new RandomListNode(0);
+        RandomListNode s1 = s;
+        while (pHead != null) {
+            RandomListNode q = pHead.next;
+            pHead.next = q.next;
+            q.next = s.next;
+            s.next = q;
+            s = s.next;
+            pHead = pHead.next;
+        }
+        return s1.next;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
