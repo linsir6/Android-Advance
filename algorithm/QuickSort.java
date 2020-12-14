54ce606605e6af1624fbf1327bfde0ff233202c0
guanweilin.6 <guanweilin.6@bytedance.com>
2020-12-14 22:23:41 +0800

    implement a simple version quick sort

--- algorithm/QuickSort.java
+++ algorithm/QuickSort.java
@@ -1,27 +1,28 @@
 public class QuickSort {
     public static void quickSort(int[] arr, int begin, int end) {
-        int tBegin = begin;
-        int tEnd = end;
-        int mid = arr[tBegin];
-
-        while (tBegin < tEnd) {
-            while (tBegin < tEnd && arr[tEnd] >= mid) {
-                tEnd--;
-            }
-            arr[tBegin] = arr[tEnd];
-            while (tBegin < tEnd && arr[tBegin] <= mid) {
-                tBegin++;
-            }
-            arr[tEnd] = arr[tBegin];
-        }
-        arr[tBegin] = mid;
-        if (begin < tEnd) {
-            quickSort(arr, begin, tEnd - 1);
-        }
-        if (tEnd < end) {
-            quickSort(arr, tEnd + 1, end);
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
         }
     }
+
+    private static int partition(int[] arr, int begin, int end) {
+        int pivot = arr[begin];
+        while (begin < end) {
+            while (begin < end && arr[end] >= pivot) {
+                end--;
+            }
+            arr[begin] = arr[end];
+            while (begin < end && arr[begin] <= pivot) {
+                begin++;
+            }
+            arr[end] = arr[begin];
+        }
+        arr[begin] = pivot;
+        return begin;
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {8, 5, 7, 6, 2, 1, 3};
