d035cc9cc9234aabe067c122679c93593f566891
guanweilin.6 <guanweilin.6@bytedance.com>
2020-12-15 23:48:28 +0800

    add mirror code

---
+++ algorithm/nowcoder/JZ18.java
@@ -0,0 +1,29 @@
+package nowcoder;
+
+public class JZ18 {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public class Solution {
+        public void Mirror(TreeNode root) {
+            if (root == null) {
+                return;
+            }
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            Mirror(root.left);
+            Mirror(root.right);
+        }
+
+
+    }
+}
