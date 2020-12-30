f2114ad793a35b581271a1cc2fe68d51d75dfebe
guanweilin.6 <guanweilin.6@bytedance.com>
2020-12-30 21:47:07 +0800

    add get ugly number

--- algorithm/nowcoder/JZ33.java
+++ algorithm/nowcoder/JZ33.java
@@ -5,10 +5,14 @@ package nowcoder;
  */
 public class JZ33 {
     public int GetUglyNumber_Solution(int index) {
-        if (index <= 0) return 0;
-        int p2 = 0, p3 = 0, p5 = 0;
+        if (index == 0) {
+            return 0;
+        }
         int[] result = new int[index];
         result[0] = 1;
+        int p2 = 0;
+        int p3 = 0;
+        int p5 = 0;
         for (int i = 1; i < index; i++) {
             result[i] = Math.min(result[p2] * 2, Math.min(result[p3] * 3, result[p5] * 5));
             System.out.print(result[i] + "\t");
@@ -16,6 +20,7 @@ public class JZ33 {
             if (result[i] == result[p3] * 3) p3++;
             if (result[i] == result[p5] * 5) p5++;
         }
+        System.out.println();
         return result[index - 1];
     }
 
