0f86c4d456aa43591be0b91d72300b0af5a32c29
guanweilin.6 <guanweilin.6@bytedance.com>
2021-01-05 21:59:00 +0800

    add find nums appear once

--- algorithm/nowcoder/JZ40.java
+++ algorithm/nowcoder/JZ40.java
@@ -1,19 +1,25 @@
 package nowcoder;
 
+/**
+ * JZ40, https://www.nowcoder.com/practice/e02fdb54d7524710a7d664d082bb7811?tpId=13&tags=&title=&diffculty=0&judgeStatus=0&rp=1
+ */
 public class JZ40 {
-
     public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
-        if (null == array) return;
-        int _OR = 0;
-        for (int temp : array) {
-            _OR ^= temp;
+        if (array == null) {
+            return;
         }
-        int classify = _OR & (-_OR);
+        int xor = 0;
+        for (int temp : array) {
+            xor = xor ^ temp;
+        }
+        int classify = xor & (-xor);
         for (int temp : array) {
             int condition = temp & classify;
             if (condition == 0) {
-                num1[0] ^= temp;
-            } else num2[0] ^= temp;
+                num1[0] = num1[0] ^ temp;
+            } else {
+                num2[0] = num2[0] ^ temp;
+            }
         }
     }
 
