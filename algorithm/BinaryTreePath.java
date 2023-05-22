3e2a0a44969135af0a62fd3560bdfbc2cef1a692
guanweilin <guanweilin@ushareit.com>
2023-05-22 13:44:37 +0800

    二叉树路径

--- algorithm/BinaryTreePath.java
+++ algorithm/BinaryTreePath.java
@@ -1,5 +1,10 @@
+package attention;
+
 import java.util.ArrayList;
 
+/**
+ * 寻找二叉树根节点到数上任意节点的路径
+ */
 public class BinaryTreePath {
 
     static class TreeNode {
@@ -13,11 +18,19 @@ public class BinaryTreePath {
     }
 
     public static boolean getPathFromRoot(TreeNode root, TreeNode node, ArrayList<Integer> pathArray) {
-        if (root == null || node == null) return false;
+        if (root == null || node == null) {
+            return false;
+        }
         pathArray.add(root.val);
-        if (root.val == node.val) return true;
-        if (getPathFromRoot(root.left, node, pathArray)) return true;
-        if (getPathFromRoot(root.right, node, pathArray)) return true;
+        if (root.val == node.val) {
+            return true;
+        }
+        if (getPathFromRoot(root.left, node, pathArray)) {
+            return true;
+        }
+        if (getPathFromRoot(root.right, node, pathArray)) {
+            return true;
+        }
         pathArray.remove((Integer) root.val);
         return false;
     }
@@ -30,8 +43,8 @@ public class BinaryTreePath {
         TreeNode n5 = new TreeNode(5);
         n1.left = n2;
         n1.right = n3;
-        n3.left = n4;
-        n3.right = n5;
+        n2.left = n4;
+        n2.right = n5;
         ArrayList<Integer> list = new ArrayList<>();
         getPathFromRoot(n1, n5, list);
         for (int i = 0; i < list.size(); i++) {
