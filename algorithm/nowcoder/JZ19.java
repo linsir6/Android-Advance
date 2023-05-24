f87ca7ba7acdc14744ca669e13eb817113383edf
guanweilin <guanweilin@ushareit.com>
2023-05-24 13:41:36 +0800

    review clock wise print matrix

--- algorithm/nowcoder/JZ19.java
+++ algorithm/nowcoder/JZ19.java
@@ -1,6 +1,7 @@
-package nowcoder;
+package attention.nowcoder;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JZ19, https://www.nowcoder.com/practice/9b4c81a02cd34f76be2659fa0d54342a?tpId=13&tags=&title=&diffculty=0&judgeStatus=0&rp=1
@@ -9,78 +10,59 @@ public class JZ19 {
 
     public static ArrayList<Integer> printMatrix(int[][] matrix) {
         ArrayList<Integer> result = new ArrayList<>();
-        int r1 = 0;
-        int r2 = matrix.length - 1;
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+        int x1 = 0;
+        int x2 = matrix[0].length - 1;
 
-        int c1 = 0;
-        int c2 = matrix[0].length - 1;
+        int y1 = 0;
+        int y2 = matrix.length - 1;
 
-        while (r1 <= r2 && c1 <= c2) {
-            for (int i = c1; i <= c2; i++) {
-                result.add(matrix[r1][i]);
+        while (x1 <= x2 && y1 <= y2) {
+            for (int i = x1; i <= x2; i++) {
+                result.add(matrix[y1][i]);
             }
-            for (int i = r1 + 1; i <= r2; i++) {
-                result.add(matrix[i][c2]);
+            for (int i = y1 + 1; i <= y2; i++) {
+                result.add(matrix[i][x2]);
             }
-            if (r1 != r2) {
-                for (int i = c2 - 1; i >= c1; i--) {
-                    result.add(matrix[r2][i]);
+            if (y1 != y2) {
+                for (int i = x2 - 1; i >= x1; i--) {
+                    result.add(matrix[y2][i]);
                 }
             }
-            if (c1 != c2) {
-                for (int i = r2 - 1; i > r1; i--) {
-                    result.add(matrix[i][c1]);
+            if (x1 != x2) {
+                for (int i = y2 - 1; i >= y1 + 1; i--) {
+                    result.add(matrix[i][x1]);
                 }
             }
-
-            r1++;
-            c1++;
-            r2--;
-            c2--;
+            x1++;
+            x2--;
+            y1++;
+            y2--;
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[][] arr = new int[5][];
-        arr[0] = new int[]{1};
-        arr[1] = new int[]{2};
-        arr[2] = new int[]{3};
-        arr[3] = new int[]{4};
-        arr[4] = new int[]{5};
+        int[][] arr = new int[3][3];
+        arr[0][0] = 1;
+        arr[0][1] = 2;
+        arr[0][2] = 3;
 
-        ArrayList<Integer> list = printMatrix(arr);
+        arr[1][0] = 4;
+        arr[1][1] = 5;
+        arr[1][2] = 6;
+
+        arr[2][0] = 7;
+        arr[2][1] = 8;
+        arr[2][2] = 9;
+
+        List<Integer> list = printMatrix(arr);
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
 
     }
 }
-//    ArrayList<Integer> temp = new ArrayList<>();
-//    int r1 = 0;
-//    int r2 = matrix.length-1;
-//    int c1 = 0;
-//    int c2 = matrix[0].length-1;
-//       while(r1<=r2&&c1<=c2){
-//               for(int i=c1;i<=c2;i++){
-//               temp.add(matrix[r1][i]);
-//               }
-//               for(int i=r1+1;i<=r2;i++){
-//               temp.add(matrix[i][c2]);
-//               }
-//               if(r1!=r2){
-//               for(int i=c2-1;i>=c1;i--)
-//               temp.add(matrix[r2][i]);
-//               }
-//               if(c1!=c2){
-//               for(int i=r2-1;i>r1;i--)
-//               temp.add(matrix[i][c1]);
-//               }
-//               r1++;
-//               r2--;
-//               c1++;
-//               c2--;
-//
-//               }
-//               return temp;
-//               }
+
