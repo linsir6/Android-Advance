d26aeee6ca7ee8d51fff69a40b941698da628725
guanweilin <guanweilin@ushareit.com>
2023-05-25 14:45:15 +0800

    review post order traversal binary tree

--- algorithm/OrderBinaryTree.java
+++ algorithm/OrderBinaryTree.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+package attention;
+
 import java.util.Stack;
 
 public class OrderBinaryTree {
@@ -70,28 +71,44 @@ public class OrderBinaryTree {
     }
 
     private static void postOrder1(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        if (root == null)
+        if (root == null) {
             return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
         TreeNode cur, pre = null;
+
         stack.push(root);
-        while (!stack.empty()) {
+
+        while (!stack.isEmpty()) {
             cur = stack.peek();
             if ((cur.left == null && cur.right == null) || (pre != null && (cur.left == pre || cur.right == pre))) {
                 TreeNode temp = stack.pop();
                 System.out.println(temp.val);
                 pre = temp;
             } else {
-                if (cur.right != null)
+                if (cur.right != null){
                     stack.push(cur.right);
-                if (cur.left != null)
+                }
+                if (cur.left != null){
                     stack.push(cur.left);
+                }
             }
         }
     }
 
     public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
 
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+
+        postOrder1(node1);
     }
 
 }
