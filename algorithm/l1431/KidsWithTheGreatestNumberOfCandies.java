a9c8f9ef22ffb107e61d05d506d741527d55fad4
guanweilin <guanweilin@ushareit.com>
2023-05-29 13:04:25 +0800

    leetcode 1431

---
+++ algorithm/l1431/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,25 @@
+package attention.l1431;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> results = new ArrayList<>();
+        int currentGreatestNumber = 0;
+        for (int i = 0; i < candies.length; i++) {
+            currentGreatestNumber = Math.max(currentGreatestNumber, candies[i]);
+        }
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies < currentGreatestNumber) {
+                results.add(false);
+            } else {
+                results.add(true);
+            }
+        }
+        return results;
+    }
+
+
+}
