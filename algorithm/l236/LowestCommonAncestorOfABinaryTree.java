3493e1835d5c8d98f5216baf7671fb492cc9ef55
guanweilin <guanweilin@ushareit.com>
2023-05-31 10:24:32 +0800

    add the lowest common ancestor of a binary tree

---
+++ algorithm/l236/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,32 @@
+package attention.l236;
+
+public class LowestCommonAncestorOfABinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null) {
+            return right;
+        }
+        if (right == null) {
+            return left;
+        }
+        return root;
+    }
+
+
+}
