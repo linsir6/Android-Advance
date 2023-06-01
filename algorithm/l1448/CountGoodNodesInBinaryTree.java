0b8cd42162f7daba6b26767b485c6f938e6b4231
guanweilin <guanweilin@ushareit.com>
2023-06-01 13:42:09 +0800

    二叉树，好节点的数量

---
+++ algorithm/l1448/CountGoodNodesInBinaryTree.java
@@ -0,0 +1,41 @@
+package attention.l1448;
+
+public class CountGoodNodesInBinaryTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public int goodNodes(TreeNode root) {
+        return dfs(root, root.val);
+    }
+
+    public int dfs(TreeNode root, int max) {
+        if (root == null) {
+            return 0;
+        }
+        int goodNum = root.val >= max ? 1 : 0;
+        max = Math.max(max, root.val);
+        goodNum += dfs(root.left, max);
+        goodNum += dfs(root.right, max);
+        return goodNum;
+    }
+
+
+}
