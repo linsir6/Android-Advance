7723e073f68cca9e2c8f5bd92e0a4984cb40e839
guanweilin <guanweilin@ushareit.com>
2023-06-01 10:38:14 +0800

    求二叉树路径总和(回溯法)

--- algorithm/l437/PathSumIII.java
+++ algorithm/l437/PathSumIII.java
@@ -48,26 +48,22 @@ public class PathSumIII {
 
 
     public int pathSum(TreeNode root, int targetSum) {
-        Map<Long, Integer> prefix = new HashMap<Long, Integer>();
-        prefix.put(0L, 1);
-        return dfs(root, prefix, 0, targetSum);
+        Map<Long, Integer> prefixPath = new HashMap<>();
+        prefixPath.put(0L, 1);
+        return dfs(root, prefixPath, 0, targetSum);
     }
 
     public int dfs(TreeNode root, Map<Long, Integer> prefix, long curr, int targetSum) {
         if (root == null) {
             return 0;
         }
-
-        int ret = 0;
-        curr += root.val;
-
-        ret = prefix.getOrDefault(curr - targetSum, 0);
+        curr = curr + root.val;
+        int result = prefix.getOrDefault(curr - targetSum, 0);
         prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
-        ret += dfs(root.left, prefix, curr, targetSum);
-        ret += dfs(root.right, prefix, curr, targetSum);
+        result += dfs(root.left, prefix, curr, targetSum);
+        result += dfs(root.right, prefix, curr, targetSum);
         prefix.put(curr, prefix.getOrDefault(curr, 0) - 1);
-
-        return ret;
+        return result;
     }
 
 
