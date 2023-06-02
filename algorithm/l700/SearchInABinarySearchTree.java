6d28f82d3398f7f9686e83a7bcff1b6a2ead6b10
guanweilin <guanweilin@ushareit.com>
2023-06-02 13:15:24 +0800

    二叉搜索树，查找

---
+++ algorithm/l700/SearchInABinarySearchTree.java
@@ -0,0 +1,30 @@
+package attention.l700;
+
+public class SearchInABinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == val) {
+            return root;
+        }
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        }else {
+            return searchBST(root.left, val);
+        }
+    }
+
+
+}
