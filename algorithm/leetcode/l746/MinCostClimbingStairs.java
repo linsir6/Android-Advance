4d8105db0401ee5cf4b0b6f1cb60616754c84d45
Liu Xinyu <linsir678@gmail.com>
2023-06-06 17:59:44 +0800

    使用最小花费爬楼梯

---
+++ algorithm/leetcode/l746/MinCostClimbingStairs.java
@@ -0,0 +1,15 @@
+package algorithm.leetcode.l746;
+
+public class MinCostClimbingStairs {
+
+    public int minCostClimbingStairs(int[] cost) {
+        int[] dp = new int[cost.length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= cost.length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[cost.length];
+    }
+
+}
