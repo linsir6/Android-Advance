40594253814428988366985d2c1aa9961b828fdd
guanweilin <guanweilin@ushareit.com>
2023-06-20 13:41:05 +0800

    反转单链表

---
+++ algorithm/leetcode/l206/ReverseLinkedList.java
@@ -0,0 +1,37 @@
+package algorithm.leetcode.l206;
+
+import java.util.List;
+
+public class ReverseLinkedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+}
