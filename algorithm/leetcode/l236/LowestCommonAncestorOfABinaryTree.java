5325c3201ca1f0198124bad84f19c65f69e009c8
linsir <linsir678@gmail.com>
2023-06-23 14:43:27 +0800

    二叉树公共祖先

--- algorithm/leetcode/l236/LowestCommonAncestorOfABinaryTree.java
+++ algorithm/leetcode/l236/LowestCommonAncestorOfABinaryTree.java
@@ -6,12 +6,7 @@ public class LowestCommonAncestorOfABinaryTree {
         int val;
         TreeNode left;
         TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
     }
-
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
@@ -19,13 +14,14 @@ public class LowestCommonAncestorOfABinaryTree {
         }
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left == null) {
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        } else {
             return right;
         }
-        if (right == null) {
-            return left;
-        }
-        return root;
     }
 
 
