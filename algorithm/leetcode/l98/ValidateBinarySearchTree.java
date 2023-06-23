f421f529d0ea350444fcf5b2c6f5f8827ce6d358
linsir <linsir678@gmail.com>
2023-06-23 14:23:30 +0800

    是否是搜索二叉树，后序方法

--- algorithm/leetcode/l98/ValidateBinarySearchTree.java
+++ algorithm/leetcode/l98/ValidateBinarySearchTree.java
@@ -22,18 +22,35 @@ public class ValidateBinarySearchTree {
     }
 
     private long preVal = Long.MIN_VALUE;
-    private boolean isValidBSTInOrder(TreeNode root){
-        if (root == null){
+
+    private boolean isValidBSTInOrder(TreeNode root) {
+        if (root == null) {
             return true;
         }
-        if (!isValidBSTInOrder(root.left)){
+        if (!isValidBSTInOrder(root.left)) {
             return false;
         }
-        if (root.val <= preVal){
+        if (root.val <= preVal) {
             return false;
         }
         preVal = root.val;
         return isValidBSTInOrder(root.right);
     }
+
+    //return dfs(root)[1] != Long.MAX_VALUE;
+    private long[] isValidBSTPostOrder(TreeNode root) {
+        if (root == null) {
+            return new long[]{Long.MAX_VALUE, Long.MIN_VALUE};
+        }
+        long[] left = isValidBSTPostOrder(root.left);
+        long[] right = isValidBSTPostOrder(root.right);
+        long x = root.val;
+        // 也可以在递归完左子树之后立刻判断，如果不是二叉搜索树，就不用递归右子树了
+        if (x <= left[1] || x >= right[0]) {
+            return new long[]{Long.MIN_VALUE, Long.MAX_VALUE};
+        }
+        return new long[]{Math.min(left[0], x), Math.max(right[1], x)};
+    }
+
 
 }
