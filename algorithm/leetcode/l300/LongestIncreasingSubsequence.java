4ca1864b05b72a2017543569fc929de9f0afa808
guanweilin <guanweilin@ushareit.com>
2023-06-27 17:27:40 +0800

    贪心+二分来实现，最长递增子序列

--- algorithm/leetcode/l300/LongestIncreasingSubsequence.java
+++ algorithm/leetcode/l300/LongestIncreasingSubsequence.java
@@ -1,6 +1,8 @@
 package algorithm.leetcode.l300;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class LongestIncreasingSubsequence {
 
@@ -18,5 +20,30 @@ public class LongestIncreasingSubsequence {
         return res;
     }
 
+    public int lengthOfLISV2(int[] nums) {
+        List<Integer> g = new ArrayList<>();
+        for (int x : nums) {
+            int j = lowerBound(g, x);
+            if (j == g.size()) g.add(x); // >=x 的 g[j] 不存在
+            else g.set(j, x);
+        }
+        return g.size();
+    }
+
+    // 开区间写法
+    private int lowerBound(List<Integer> g, int target) {
+        int left = -1, right = g.size(); // 开区间 (left, right)
+        while (left + 1 < right) { // 区间不为空
+            // 循环不变量：
+            // nums[left] < target
+            // nums[right] >= target
+            int mid = (left + right) >>> 1;
+            if (g.get(mid) < target)
+                left = mid; // 范围缩小到 (mid, right)
+            else
+                right = mid; // 范围缩小到 (left, mid)
+        }
+        return right; // 或者 left+1
+    }
 
 }
